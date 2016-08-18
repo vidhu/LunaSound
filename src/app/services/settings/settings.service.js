3e4e92097b6934f90cf2d433fdf0af2b3cd77650
vidhu <vidhu@bu.edu>
2016-08-19 03:34:24 +0400

    Changed settings database to config.json

--- src/app/services/settings/settings.service.js
+++ src/app/services/settings/settings.service.js
@@ -1,65 +1,46 @@
 (function(){
     'use strict'
-    const JSONPath = require('jsonpath-plus');
+    const path = require('path');
+    const nconf = require('nconf');
 
     angular.module('LunaSound.Settings')
         .factory('settings', settingsService);
 
-    function settingsService(localStorageService){
+    function settingsService(){
+        let configPath = path.join(process.env.BASE_DIR, 'config.json');
 
+        nconf.argv()
+            .env()
+            .file({ file: configPath });
 
         var API = {
             getSettings: function(){
-                var settings = localStorageService.get('settings');
-                if(settings == undefined)
-                    settings = {};
-                return settings;
+                return nconf.get();
             },
             getSetting: function(key){
-                var settings = API.getSettings();
-                var result = JSONPath({json: settings, path: key});
-                if(result.length == 0)
-                    return null;
-                else
-                    return result[0];
+                return nconf.get(key);
             },
-            saveSettings: function(s){
-                localStorageService.set('settings', s);
+            saveSettings: function(){
+                nconf.save(function (err) {
+                    fs.readFile(configPath, function (err, data) {
+                        console.dir(JSON.parse(data.toString()))
+                    });
+                });
             },
             saveSetting: function(path, setting){
-                var settings = API.getSettings();
+                nconf.set(path, setting);
 
-                var keys = path.split('.');
-
-                var curPos = settings;
-                for(let i=0; i<keys.length; i++){
-                    var key = keys[i];
-
-                    //Check if we are are leaf
-                    if(i == keys.length-1) {
-                        curPos[key] = setting;
-                        break;
-                    }
-
-                    //If key doesn't exist, add it
-                    if(!(key in curPos))
-                        curPos[key] = {};
-
-                    //update pointer
-                    curPos = curPos[key];
-                }
-
-                API.saveSettings(settings);
+                API.saveSettings();
             },
             getMusicDir: function(){
-                var musicDir = API.getSetting('dir.music');
+                var musicDir = API.getSetting('dir:music');
                 if(musicDir == null || musicDir == '')
                     musicDir = process.env.MUSIC_DIR;
                 API.saveMusicDir(musicDir);
                 return musicDir;
             },
             saveMusicDir: function(dir){
-                API.saveSetting('dir.music', dir);
+                API.saveSetting('dir:music', dir);
             }
         };
 
