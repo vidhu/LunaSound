b9c1c665c06d6a07f58aa7d0342d2d491399b1f5
vidhu <vidhu@bu.edu>
2016-08-24 17:30:37 +0400

    Removed linux from gulp build for now

--- gulpfile.js
+++ gulpfile.js
@@ -37,7 +37,7 @@ function build() {
             cache: './cache',
             version: 'v1.2.6',
             packaging: false,
-            platforms: ['win32-ia32', 'linux-x64']
+            platforms: ['win32-ia32']
         }))
         .pipe(gulp.dest(''));
 }
